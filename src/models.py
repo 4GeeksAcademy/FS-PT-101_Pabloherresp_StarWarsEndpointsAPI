448814511a8ba4fe5bc4a317e0f8a04e7e5bd3d3
Pabloherresp <pabloherreraespinola@gmail.com>
2025-05-13 23:40:53 +0000

    First solution

--- src/models.py
+++ src/models.py
@@ -1,19 +1,75 @@
+import enum
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Type(enum.Enum):
+    Fire = "Fire"
+    Water =  "Water"
+    Grass = "Grass"
+    Bug = "Bug"
+    Dark = "Dark"
+    Dragon = "Dragon"
+    Electric = "Electric"
+    Fairy = "Fairy"
+    Fighting = "Fighting"
+    Flying = "Flying"
+    Ghost = "Ghost"
+    Ground = "Ground"
+    Ice = "Ice"
+    Normal = "Normal"
+    Poison = "Poison"
+    Psychic = "Psychic"
+    Rock = "Rock"
+    Steel = "Steel"
+
+
+class Users(db.Model):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favs: Mapped[list["Favs"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
         }
+
+class Pokemons(db.Model):
+    __tablename__ = "pokemons"
+    id:  Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    type : Mapped[Type] = mapped_column(Enum(Type), nullable=False)
+    favved_by: Mapped[list["Favs"]] = relationship(back_populates="pokemon")
+
+class Cities(db.Model):
+    __tablename__ = "cities"
+    id:  Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    region_id: Mapped[int] = mapped_column(ForeignKey("regions.id"))
+    region: Mapped["Regions"] = relationship(back_populates="cities", uselist=False)
+    favved_by: Mapped[list["Favs"]] = relationship(back_populates="city")
+
+class Regions(db.Model):
+    __tablename__ = "regions"
+    id:  Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    cities: Mapped[list["Cities"]] = relationship(back_populates="region")
+    favved_by: Mapped[list["Favs"]] = relationship(back_populates="region")
+
+class Favs(db.Model):
+    __tablename__ = "favs"
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"),primary_key=True)
+    pokemon_id: Mapped[int] = mapped_column(ForeignKey("pokemons.id"),primary_key=True)
+    city_id: Mapped[int] = mapped_column(ForeignKey("cities.id"),primary_key=True)
+    region_id: Mapped[int] = mapped_column(ForeignKey("regions.id"),primary_key=True)
+
+    user: Mapped["Users"] = relationship(back_populates="favs")
+    pokemon: Mapped["Pokemons"] = relationship(back_populates="favved_by")
+    city: Mapped["Cities"] = relationship(back_populates="favved_by")
+    region: Mapped["Regions"] = relationship(back_populates="favved_by")
---
+++ src/seed.py
@@ -0,0 +1,38 @@
+from app import app, db
+from models import Users, Pokemons, Cities, Regions, Favs, Type
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    user1 = Users(email="pablo@example.com", password="1234")
+    user2 = Users(email="rocky@example.com", password="5678")
+    db.session.add_all([user1, user2])
+    db.session.commit()
+
+    pokemon1 = Pokemons(name="Pikachu", type=Type.Electric)
+    pokemon2 = Pokemons(name="Charmander", type=Type.Fire)
+    db.session.add_all([pokemon1, pokemon2])
+    db.session.commit()
+
+    region1 = Regions(name="Kanto")
+    region2 = Regions(name="Johto")
+    db.session.add_all([region1, region2])
+    db.session.commit()
+
+    # Cursos
+    city1 = Cities(name="Pueblo Paleta", region_id=region1.id)
+    city2 = Cities(name="Pueblo Azalea", region_id=region2.id)
+    db.session.add_all([city1, city2])
+    db.session.commit()
+
+    # fav1 = Favs(user_id=user1.id,city_id=city1.id)
+    # fav2 = Favs(user_id=user1.id,city_id=city2.id)
+    # fav3 = Favs(user_id=user1.id,pokemon_id=pokemon2.id)
+    # fav4 = Favs(user_id=user2.id,region_id=region1.id)
+    # fav5 = Favs(user_id=user2.id,pokemon_id=pokemon1.id)
+    # fav6 = Favs(user_id=user2.id,city_id=city1.id)
+    # db.session.add_all([fav1, fav2, fav3, fav4, fav5, fav6])
+
+    # db.session.commit()
+    print("✅ Datos sembrados correctamente.")
